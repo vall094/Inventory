fba2900a0111b36248b3183756894c02d33a5fcd
Novalf <novalfajarbudipamungkas@gmail.com>
2024-09-23 17:30:03 +0700

    Tugas Inventory

---
+++ app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.inventory.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemsRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(itemsRepository: ItemsRepository): ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> =
+        itemsRepository.getAllItemsStream().map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * Ui State for HomeScreen
+ */
+data class HomeUiState(val itemList: List<Item> = listOf())
